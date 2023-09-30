9da33c5c3c77b14466543124a99364d716df391f
STRI_DEV <41266421+VVAIZU@users.noreply.github.com>
2023-09-30 19:31:27 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,22 +1,28 @@
-#6372475462:AAHqvbkS5n-4p4J5oqXx0D9o9uAoWgomk8A
-#accountant.db
-import sqlite3
 import telebot
+import psycopg2
 from telebot import types
+import os
+
+# Получите URL вашей базы данных PostgreSQL из переменной окружения на платформе Railway
+DATABASE_URL = os.environ.get("postgresql://postgres:Rt3Pjsqg0hyocZArFw2Z@containers-us-west-86.railway.app:6042/railway")
 
 # Инициализируем Telegram бота
 bot = telebot.TeleBot('6372475462:AAHqvbkS5n-4p4J5oqXx0D9o9uAoWgomk8A')
+
+try:
+    # Подключаемся к базе данных PostgreSQL
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    print("Connected to PostgreSQL")
+except psycopg2.OperationalError as e:
+    print(f"Failed to connect to PostgreSQL: {e}")
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Добро пожаловать! Выберите учебник:")
 
-    # Создаем подключение к базе данных SQLite и объект cursor
-    conn = sqlite3.connect('accountant.db')
-    cursor = conn.cursor()
-
-    # Получаем список учебников из базы данных
+    # Запрос к базе данных PostgreSQL для получения списка учебников
     cursor.execute("SELECT id, name FROM textbooks")
     textbooks = cursor.fetchall()
 
@@ -29,22 +35,13 @@ def send_welcome(message):
 
     bot.send_message(message.chat.id, text='Выберите учебник:', reply_markup=keyboard)
 
-    # Закрываем соединение с базой данных
-    cursor.close()
-    conn.close()
-
-# Другие обработчики сообщений и команд
 # Обработчик нажатий на кнопки учебников
 @bot.callback_query_handler(lambda call: call.data.startswith('textbook_'))
 def handle_textbook_query(call):
     textbook_id = int(call.data.split('_')[1])
 
-    # Создаем подключение к базе данных SQLite и объект cursor
-    conn = sqlite3.connect('accountant.db')
-    cursor = conn.cursor()
-
-    # Получаем список задач для выбранного учебника
-    cursor.execute("SELECT id, name FROM tasks WHERE textbook_id = ?", (textbook_id,))
+    # Запрос к базе данных PostgreSQL для получения списка задач для выбранного учебника
+    cursor.execute("SELECT id, name FROM tasks WHERE textbook_id = %s", (textbook_id,))
     tasks = cursor.fetchall()
 
     # Создаем InlineKeyboardMarkup с кнопками для выбора задачи
@@ -56,21 +53,13 @@ def handle_textbook_query(call):
 
     bot.send_message(call.message.chat.id, text='Выберите задачу:', reply_markup=keyboard)
 
-    # Закрываем соединение с базой данных
-    cursor.close()
-    conn.close()
-
 # Обработчик нажатий на кнопки задач
 @bot.callback_query_handler(lambda call: call.data.startswith('task_'))
 def handle_task_query(call):
     task_id = int(call.data.split('_')[1])
 
-    # Создаем подключение к базе данных SQLite и объект cursor
-    conn = sqlite3.connect('accountant.db')
-    cursor = conn.cursor()
-
-    # Получаем текст решения и URL изображения для выбранной задачи из базы данных
-    cursor.execute("SELECT name, solution, image_url FROM tasks WHERE id = ?", (task_id,))
+    # Запрос к базе данных PostgreSQL для получения информации о выбранной задаче
+    cursor.execute("SELECT name, solution, image_url FROM tasks WHERE id = %s", (task_id,))
     task_data = cursor.fetchone()
 
     if task_data:
@@ -81,10 +70,6 @@ def handle_task_query(call):
             bot.send_photo(call.message.chat.id, photo=image_url)  # Отправляем изображение
     else:
         bot.send_message(call.message.chat.id, text='Решение задачи не найдено.')
-
-    # Закрываем соединение с базой данных
-    cursor.close()
-    conn.close()
 
 # Запускаем бота
 bot.polling()
